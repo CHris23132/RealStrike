d5a047756c9cdf6405cfefdf072454fe229be3ef
CHris23132 <play3dinc@gmail.com>
2025-04-05 01:04:36 -0400

    Initial Commit

---
+++ RealStrike-Warfare/RealStrike_WarfareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealStrike_WarfareApp.swift
+//  RealStrike-Warfare
+//
+//  Created by Chris on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct RealStrike_WarfareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
